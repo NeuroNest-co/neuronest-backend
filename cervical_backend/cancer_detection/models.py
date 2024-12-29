ea669354ebccfb22fbfdbdd583e3a4e762e7c71b
Meman Salam <memansalam@IdeaPad-Slim-3-15IRH8.localdomain>
2024-12-29 14:53:29 +0200

    add onboarding patient

--- cervical_backend/cancer_detection/models.py
+++ cervical_backend/cancer_detection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Patient(models.Model):
+    patientId = models.AutoField(primary_key=True)
+    age = models.PositiveIntegerField()
+    date = models.DateTimeField(default=now)
+    doctor_comment = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.patientId
+
